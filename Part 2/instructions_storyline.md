4ec26a7f9c03c2f90f67e207c4c35a3b59028c95
Joel Leo <leo.qiyi.joel@dhs.sg>
2020-05-09 14:25:20 +0800

    Updated noPipeline.py

--- Part 2/instructions_storyline.md
+++ Part 2/instructions_storyline.md
@@ -1,4 +1,4 @@
-# Storyline ... test
+# Storyline ...
 
 Our sacred BuildingBloCS logo has been stolen! And the culprit is currently still at large somewhere in BuildingBloCS land :scream:
 
--- Part 2/noPipeline.py
+++ Part 2/noPipeline.py
@@ -1,29 +1,27 @@
-import pymongo
-from pymongo import MongoClient
-print("import successful")
-cluster = MongoClient("mongodb+srv://test1:test1@cluster0-d9jjq.mongodb.net/test?retryWrites=true&w=majority")
-print("connected")
-#mongodb+srv://test1:test1@cluster0-d9jjq.mongodb.net/test?retryWrites=true&w=majority
-db = cluster["finaltest"] #name of database is test
-collection = db["test1"] #name of collection is test
+#installing python packages
+import _____
+from _____ import ______
+uri = "_________________________________" #add in your MongoDB connection string here
+cluster = MongoClient(uri)
+
+db = cluster["__________"] #insert name of database you created earlier here
+collection = db["__________"] #insert name of collection you created earlier here
 
 pipeline = [
-    {
-    "$match": {"$and" :
-        [{"age": {"$gte": 20, "$lt": 30}},
-        {"company": {"$in": ["PRINTSPAN", "TECHMANIA", "NEPTIDE", "MULTRON", "SKYNET", "UPDAT", "STANTON"]}},
-        {"eyeColor": {"$nin": ["black", "brown"]}},
-        {"friends.name": {"$regex": "^C"}}
+    {"$___": {"$___" : [
+        {"______": {"$___": 20, "__": 30}},
+        {"______": {"$___": ["PRINTSPAN", "TECHMANIA", "NEPTIDE", "MULTRON", "SKYNET", "UPDAT", "STANTON"]}},
+        {"______": {"$___": ["black", "brown"]}},
+        {"______": {"$___": "^C"}}
     ]}},
-    {"$project": {"name": 1, "_id": 0}}
+    {"$____":
+        {"______": 1, "______": 0}}
 ]
 
-result = collection.aggregate(pipeline)
+result = collection.______(pipeline)
 for i in result:
     print (i)
 
-print("is this working?")
-print("ending")
 
 #https://www.gitpod.io/blog/gitpodify/
 #https://www.gitpod.io/docs/getting-started/
